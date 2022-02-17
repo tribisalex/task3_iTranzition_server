3229187829d614f422196c10a97c540fbb54442d
Alexandr <tribisalexandr@yandex.ru>
2022-02-18 02:27:04 +0300

    fix errors

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: process.env.POSTGRES_USERNAME || process.env.DB_USER,
-  password: process.env.POSTGRES_PASSWORD || process.env.DB_PASSWORD,
-  host: process.env.POSTGRES_HOST || process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database:  process.env.POSTGRES_DATABASE || process.env.DB_NAME
+  user: 'postgres',
+  password: 'root',
+  host: 'localhost',
+  port: 5432,
+  database: 'iTransition'
 })
 
 module.exports = pool
--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const router = require('./routes/index')
-const errorHandler = require('./middleware/ErrorHandingMiddleware')
 const cors = require('cors')
-const corsOptions ={
-  origin:'https://tribisalex.github.io/task3_iTranzition_client/',
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
+const errorHandler = require('./middleware/ErrorHandingMiddleware')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
 app.use(express.json())
-app.use(cors(corsOptions));
 app.use('/api', router)
-
-
 
 
 app.use(errorHandler)
