c1c3741eea375ade4d71ce871bdc86bbe0f4a4e0
Alexandr <tribisalexandr@yandex.ru>
2022-02-18 00:08:28 +0300

    fix errors with CORS

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandingMiddleware')
 const cors = require('cors')
 const corsOptions ={
-  origin:'http://localhost:3000',
+  origin:'https://tribisalex.github.io/task3_iTranzition_client/',
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
-
-
 
 const PORT = process.env.PORT || 5000
 
