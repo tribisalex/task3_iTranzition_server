4837c932d937d299adf7acd9d4b0dc27ea9a3ea4
Alexandr <tribisalexandr@yandex.ru>
2022-02-17 21:26:57 +0300

    fix errors

--- dataBase.sql
+++ dataBase.sql
@@ -1,7 +1,7 @@
 create TABLE users (
 id SERIAL PRIMARY KEY,
 name VARCHAR(255),
-email VARCHAR(255) unique: true,
+email VARCHAR(255) unique,
 dateRegistration VARCHAR(255),
 dateLastAuthorization VARCHAR(255),
 password VARCHAR(255),
--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
+  user: process.env.POSTGRES_USERNAME || process.env.DB_USER,
+  password: process.env.POSTGRES_PASSWORD || process.env.DB_PASSWORD,
+  host: process.env.POSTGRES_HOST || process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  database:  process.env.POSTGRES_DATABASE || process.env.DB_NAME
 })
 
 module.exports = pool
